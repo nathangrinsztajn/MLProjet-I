06dcc7dc567adbd2f212e8643c21562615754be9
Ultiss <nathan.grinsztajn@polytechnique.edu>
2017-11-17 18:01:39 +0100

    ajout deep learning

--- Gini.py
+++ Gini.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.metrics import roc_auc_score
 
 #Défnit coefficients de Gini
 
@@ -15,3 +16,36 @@ def gini(actual, pred, cmpcol=0, sortcol=1):
 
 def gini_normalized(a, p):
     return gini(a, p) / gini(a, a)
+
+def giniMetric(actual, pred, cmpcol=0, sortcol=1):
+    assert (len(actual) == len(pred))
+    all = np.asarray(np.c_[actual, pred, np.arange(len(actual))], dtype=np.float)
+    all = all[np.lexsort((all[:, 2], -1 * all[:, 1]))]
+    totalLosses = all[:, 0].sum()
+    giniSum = all[:, 0].cumsum().sum() / totalLosses
+    giniSum -= (len(actual) + 1) / 2.
+    return giniSum / len(actual)
+
+
+
+#Calculs plus rapides
+def ginic(actual, pred):
+    actual = np.asarray(actual)  # In case, someone passes Series or list
+    n = len(actual)
+    a_s = actual[np.argsort(pred)]
+    a_c = a_s.cumsum()
+    giniSum = a_c.sum() / a_s.sum() - (n + 1) / 2.0
+    return giniSum / n
+
+
+def gini_normalizedc(a, p):
+    #if p.ndim == 2:  # Required for sklearn wrapper
+    #   p = p[:, 1]  # If proba array contains proba for both 0 and 1 classes, just pick class 1
+    return ginic(a, p) / ginic(a, a)
+
+a = [0]*50+[1]*50
+b = np.random.uniform(low=0, high=1, size=100)
+print(b)
+print((roc_auc_score(a, b) * 2 ) - 1)
+print(gini_normalizedc(a, b))
+print(gini_normalized(a, b))
--- Main.py
+++ Main.py
@@ -4,11 +4,18 @@ from data import *
 import pandas as pd
 from GestionNA import *
 from PreProcess import *
-import sklearn
 from sklearn.model_selection import train_test_split, KFold, cross_val_score
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
-
+from sklearn.metrics import log_loss, roc_auc_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import load_model
+from keras.models import Sequential, Model
+from keras.layers import Input, Dense, Dropout, Activation
+from keras.layers.normalization import BatchNormalization
+from keras.callbacks import EarlyStopping, ModelCheckpoint, CSVLogger, Callback
+from keras.wrappers.scikit_learn import KerasClassifier
 
 train = dropNA(train)
 print(train.shape)
@@ -27,17 +34,47 @@ X_test = X_test.as_matrix()
 y_test = y_test.as_matrix()
 
 # create model
+
+def baseline_model():
+    model = Sequential()
+    model.add(
+        Dense(
+            200,
+            input_dim=X_train.shape[1],
+            kernel_initializer='glorot_normal',
+        ))
+    model.add(Activation('relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
+    model.add(Dense(100, kernel_initializer='glorot_normal'))
+    model.add(Activation('relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.25))
+    model.add(Dense(50, kernel_initializer='glorot_normal'))
+    model.add(Activation('relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.15))
+    model.add(Dense(25, kernel_initializer='glorot_normal'))
+    model.add(Activation('relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.1))
+    model.add(Dense(1, activation='sigmoid'))
+
+    # Compile model
+    model.compile(optimizer='adam', metrics=['accuracy'], loss='binary_crossentropy')
+    return model
+
 model = Sequential()
-model.add(Dense(200, input_dim=X_train.shape[1], activation='relu'))
-model.add(Dense(100, activation='relu'))
+model.add(Dense(1000, input_dim=X_train.shape[1], activation='relu'))
+model.add(Dense(500, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
 # compile model
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy', gini_normalized])
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=[gini_normalizedc])
 
 
 # Fit the model
-model.fit(X_train, y_train, nb_epoch=400, batch_size=32,  verbose=2)
+model.fit(X_train, y_train, nb_epoch=700, batch_size=32,  verbose=2)
 
 yPred = model.predict(X_test)
 print("yPred = ")
